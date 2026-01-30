e206830602a69aceb7228d1ff03ede6d116843e1
rennanpaulo1000-afk <rennanpaulo1000@gmail.com>
2026-01-30 19:26:28 -0300

    Update main.py

--- main.py
+++ main.py
@@ -26,7 +26,12 @@ def buscar(produto: str):
     }
 
     try:
-        response = requests.get(url, params=params, timeout=10)
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0 Safari/537.36"
+}
+
+response = requests.get(url, params=params, headers=headers, timeout=10)
+
         response.raise_for_status()
         data = response.json()
 
